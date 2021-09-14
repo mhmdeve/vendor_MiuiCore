92e4ea3e112f2aeff46759a7f29b9fb3492de471
adazem009 <info@adazem.com>
2021-09-14 06:38:47 +0000

    Fix miuisystem

--- system/app/Android.mk
+++ system/app/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := miui
+LOCAL_MODULE := miuisystem
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := miuisystem/miuisystem.apk
 LOCAL_CERTIFICATE := platform
